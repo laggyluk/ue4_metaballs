297556ed983e01d7c77528eb442b4f391d9d4737
andyrst <jocker3d@gmail.com>
2016-03-17 09:19:13 +0300

    Initial

---
+++ Plugin/Source/MetaballsPlugin/Public/MetaballsPluginPrivatePCH.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "CoreUObject.h"
+#include "Engine.h"
+#include "GameFramework/MovementComponent.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/Pawn.h"
+#include "ProceduralMeshComponent.h"
